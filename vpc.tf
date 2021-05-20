8955f4f8d43fee9fff896bbbcf92f07047279f5d
Miguel Angel Garcia <miguelangr@gmail.com>
2021-05-20 16:30:14 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/miguelangel-training/network/google"
   version = "2.5.0"
   # insert required variables here
-network_name = miguelangel-network 
+network_name = "miguelangel-network" 
 project_id= var.project
 subnets = [
   {
